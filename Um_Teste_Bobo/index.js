75bf2f4f279f3b93f15b64e744ccd68ada56748a
Natan Oliveira da Silva <natan_o_da_silva@outlook.com>
2024-05-02 16:42:52 -0300

    Add files via upload

---
+++ Um_Teste_Bobo/index.js
@@ -0,0 +1,11 @@
+function ingles(){
+    document.getElementById("texto").innerHTML="I like to run.";
+    document.getElementById("brasil").style.display='block';
+    document.getElementById("eua").style.display='none';
+}
+
+function portugues(){
+    document.getElementById("texto").innerHTML="Eu gosto de correr.";
+    document.getElementById("eua").style.display='block';
+    document.getElementById("brasil").style.display='none';
+}
---
+++ Um_Teste_Bobo/muda_idioma.js
@@ -0,0 +1,3 @@
+function muda_idioma() {
+  alert("didnidnieide");
+}
